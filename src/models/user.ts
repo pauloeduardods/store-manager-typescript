9602f36eb3b442325100b6e6aad32a0c258fa3d1
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-02-23 15:00:08 -0300

    Try to fix evaluator error

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,7 +4,10 @@ import { User } from '../interfaces/user';
 
 export async function create(user: User): Promise<number> {
   try {
-    const sql = 'INSERT INTO Users (username, password, level, classe) VALUES (?, ?, ?, ?)';
+    const sql = `
+      INSERT INTO Trybesmith.Users (username, password, level, classe)
+      VALUES (?, ?, ?, ?)
+    `;
     const values = [user.username, user.password, user.level, user.classe];
     const [res]: [ResultSetHeader, FieldPacket[]] = await mysql.execute(sql, values);
     return res.insertId;
