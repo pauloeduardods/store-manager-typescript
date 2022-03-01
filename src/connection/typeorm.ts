c5984cd2bc02c9129bacf540dad10d90f1f53938
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-03-01 14:02:17 -0300

    Try to fix error in evaluator

--- src/connection/typeorm.ts
+++ src/connection/typeorm.ts
@@ -1,7 +1,18 @@
 import dotenv from 'dotenv';
-import { createConnection, ConnectionOptions } from 'typeorm';
-import config from '../../ormconfig';
+import { createConnection } from 'typeorm';
 
 dotenv.config();
 
-export default async () => createConnection(config as ConnectionOptions);
+export default async () => createConnection({
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: 'Trybesmith',
+  synchronize: false,
+  logging: false,
+  entities: [
+    'src/entity/*{.ts,.js}',
+  ],
+});
